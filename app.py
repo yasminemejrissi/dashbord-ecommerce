c6a894dc22ac978366e276f7d146cd658db627f3
yasminemejrissi <yasmine.mejrissi@outlook.com>
2025-04-15 00:21:17 +0200

    Ajout du fichier app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+#streamlit rappel données fictives: 
+data = {
+    "profil":["VIP", "Promo addict", "Régulier", "Occasionnel"],
+    "nb_clients":[28, 19, 32, 21],
+    "nb_commandes":[1600,1728,1485,792],
+    "panier_moyen":[650.12, 550.20, 510.00, 420.40],
+    "ratio_code_promos":[0.68,0.82,0.65,0.72],
+    "chiffre_affaires":[102000, 95000, 78000, 32000]
+
+}
+
+#Création du dataframe
+df=pd.DataFrame(data)
+
+st.title("Dashboard comportement clients")
+st.subheader("Tableau comparatif des KPIs")
+st.dataframe(df.style.highlight_max(axis=0))
+
+#Camembert profils
+st.subheader("Répartition des profils")
+fig1,ax1=plt.subplots()
+df["profil"].value_counts().plot(kind="pie",autopct="%1.1f%%",startangle=90,ax=ax1)
+st.pyplot(fig1)
+
+#Graphiques en barre pour les chiffres d'affaires par rapport aux profils
+fig2,ax2=plt.subplots()
+df.sort_values("chiffre_affaires",ascending=True).plot(kind="barh",color="salmon",ax=ax2)
+st.pyplot(fig2)
+
+#Panier moyen par profil
+st.subheader("Panier moyen par profil")
+fig3,ax3=plt.subplots()
+df.sort_values("panier_moyen",ascending=True).plot(kind="barh",color="skyblue",ax=ax3)
+st.pyplot(fig3)
+
+
+st.markdown("**Projet e-commerce réalisé par Yasmine M.✨**")
